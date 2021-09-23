29649104a8d234c4a0c7c914c23264d89e8e0223
未己 <weiji@qunhemail.com>
2021-09-23 14:50:42 +0800

    ✨ feat: useLockScroll

--- src/hooks/useLockScroll/Demo/index.tsx
+++ src/hooks/useLockScroll/Demo/index.tsx
@@ -2,10 +2,14 @@ import React, { useRef } from 'react';
 import { useLockScroll } from 'wwhooks';
 
 export default () => {
-    const domRef = useRef<any>()
-    const [lock, toggle] = useLockScroll(domRef.current)
+    const domRef = useRef<HTMLDivElement>()
+    const [toggle, lockState] = useLockScroll(domRef.current)
+    const [toggleBody, lockBodyState] = useLockScroll()
     return <>
-        <button onClick={toggle}>lock</button>
+        <p>
+            <button onClick={toggle}>lock:{lockState.toString()}</button>
+            <button onClick={toggleBody}>lockBody:{lockBodyState.toString()}</button>
+        </p>
 
         <div style={{ height: 400, overflow: 'auto', border: `1px solid black` }} ref={domRef}>
             <div style={{ height: 1000 }}>
--- src/hooks/useLockScroll/index.ts
+++ src/hooks/useLockScroll/index.ts
@@ -1,38 +1,30 @@
-import { numToPx, pxToNum } from '@/common/utils';
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useBoolean from '../useBoolean';
 
 const lockScrollStyle = {
-  // width: `calc(100% - 17px)`,
   overflowY: `hidden`,
-  boxSizing: `border-box`,
 };
 export function useLockScroll(container?: HTMLElement) {
   const [lock, { toggle }] = useBoolean(false);
 
   useEffect(() => {
     container = container || document.body;
-    const {
-      paddingRight: originalPaddingRight,
-      overflow: originalOverflow,
-      boxSizing: originalBoxSizing,
-    } = container.style;
+    const { paddingRight: originalPaddingRight, overflow: originalOverflow } =
+      container.style;
 
     const { width } = container.getBoundingClientRect();
 
     if (lock) {
       container.style.width = `${width}px`;
       container.style.overflowY = lockScrollStyle.overflowY;
-      container.style.boxSizing = lockScrollStyle.boxSizing;
     }
 
     return () => {
       container.style.overflow = originalOverflow;
       container.style.width = originalPaddingRight;
-      container.style.boxSizing = originalBoxSizing;
     };
   }, [lock, container]);
-  return [lock, toggle];
+  return [toggle, lock] as const;
 }
 
 export default useLockScroll;
