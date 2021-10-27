267f0df9c7effda264eac30186d663e63ce66bec
404174262@qq.com <404174262@qq.com>
2021-10-27 18:41:22 +0800

    ✨ feat: add dataType function

---
+++ src/function/IsDataType/Demo/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import isType from '..';
+
+const App = () => {
+  const log = JSON.stringify;
+  const text = 'test';
+  const isString = isType<string>(text, 'string');
+  const array = new Array(1, 2, 3);
+  const isArray = isType<any[]>(array, 'array');
+  return (
+    <>
+      <p>{log(isString)}</p>
+      <p>{log(isArray)}</p>
+    </>
+  );
+};
+
+export default App;
