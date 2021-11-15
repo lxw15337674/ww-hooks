e491bcf28ee59eb7a519b72e267315d50d9285dd
404174262@qq.com <404174262@qq.com>
2021-11-15 15:28:08 +0800

    update template

---
+++ template/Demo/index.tsx
@@ -0,0 +1,13 @@
+/**
+ *
+ * title: 轮询
+ * desc: |
+ *  通过设置 `options.pollingInterval` ，进入轮询模式，定时触发函数执行。
+ *  - 通过 `run` / `cancel` 来 开启/停止 轮询。
+ *
+ */
+import React, { useState } from 'react';
+const test = (props) => {
+  return <div>test</div>;
+};
+export default test;
--- template/index.ts
+++ template/index.ts
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-
-export function useLockScroll(lock: boolean, container?: HTMLElement) {
-  return useState;
-}
-
-export default useLockScroll;
+const test = (props) => {
+  return null;
+};
+export default test;
