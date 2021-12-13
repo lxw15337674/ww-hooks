c1e4de587793bedd1171c7fcbb018a282d200406
404174262@qq.com <404174262@qq.com>
2021-12-13 17:45:33 +0800

    useIntersectionObserver

---
+++ src/hooks/useIntersectionObserver/index.ts
@@ -0,0 +1,30 @@
+import { getTargetElement } from '../../common/dom';
+import { useEffect, useRef } from 'react';
+import { BasicTarget } from '../../common/interface';
+
+function useIntersectionObserver<T extends Element = Element>(
+  target: BasicTarget<T>,
+  callback: (info: IntersectionObserverEntry) => void,
+  config?: IntersectionObserverInit,
+) {
+  const ref = useRef(
+    new IntersectionObserver((entries) => {
+      for (const entry of entries) {
+        callback(entry);
+      }
+    }, config),
+  );
+  const observer = ref.current;
+  useEffect(() => {
+    const el = getTargetElement(target);
+    if (!el) {
+      return;
+    }
+    observer.observe(el as Element);
+    return () => {
+      observer.disconnect();
+    };
+  }, [target]);
+}
+
+export default useIntersectionObserver;
