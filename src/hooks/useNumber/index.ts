8c2ceffba40db050a6b6c68d410ee4019f6aa574
404174262@qq.com <404174262@qq.com>
2022-03-08 16:28:07 +0800

    useNumber add max、min

--- src/hooks/useNumber/index.ts
+++ src/hooks/useNumber/index.ts
@@ -1,24 +1,44 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import usePersistFn from '../usePersistFn';
 
-const useNumber = (defaultValue: number = 0) => {
-  const [number, setNumber] = useState<number>(defaultValue);
+interface Options {
+  min?: number;
+  max?: number;
+}
 
-  const actions = useMemo(() => {
-    const plus = (v: number = 1) => {
-      setNumber((c) => c + v);
-    };
-    const minus = (v: number = 1) => {
-      setNumber((c) => c - v);
-    };
-    return { plus, minus };
-  }, []);
+function getTargetValue(val: number, options: Options = {}) {
+  const { min, max } = options;
+  let target = val;
+  if (typeof max === 'number') {
+    target = Math.min(max, target);
+  }
+  if (typeof min === 'number') {
+    target = Math.max(min, target);
+  }
+  return target;
+}
+
+const useNumber = (defaultValue = 0, config?: Options) => {
+  const [number, setNumber] = useState<number>(
+    getTargetValue(defaultValue, config),
+  );
+
+  const setValue = usePersistFn((v) => {
+    setNumber(getTargetValue(v, config));
+  });
+
+  const plus = usePersistFn((v: number = 1) => {
+    setNumber((c) => c + v);
+  });
+  const minus = usePersistFn((v: number = 1) => {
+    setNumber((c) => c - v);
+  });
 
   const reset = usePersistFn(() => {
     setNumber(defaultValue);
   });
 
-  return [number, { set: setNumber, reset, ...actions }] as const;
+  return [number, { set: setValue, reset, plus, minus }] as const;
 };
 
 export default useNumber;
---
+++ src/hooks/useSessionStorage/Demo/index.tsx
@@ -0,0 +1,10 @@
+/**
+ * title: 用法
+ * desc: 通过ref监听节点变化
+ */
+import React, { useRef } from 'react';
+import { useScroll } from 'wwhooks';
+
+export default () => {
+  return null;
+};
