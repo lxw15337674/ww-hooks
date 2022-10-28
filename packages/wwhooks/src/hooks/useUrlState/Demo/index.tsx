4c13e489a7fe7b0411ce2fef29316b75bffd7207
404174262@qq.com <404174262@qq.com>
2022-10-28 14:27:44 +0800

    fix func

--- packages/wwhooks/src/hooks/useUrlState/Demo/index.tsx
+++ packages/wwhooks/src/hooks/useUrlState/Demo/index.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import { useUrlState, useToggle } from 'wwhooks';
 
 export default () => {
-  const [navigateMode, { toggle }] = useToggle<'push' | 'replace'>('push', 'replace');
+  const [navigateMode, toggle] = useToggle<'push' | 'replace'>(
+    'push',
+    'replace',
+  );
   const [state, actions] = useUrlState({ count: '1', test: '2' }, navigateMode);
   return (
     <>
       <p>state:{state.get('count')}</p>
       <p>navigateMode:{navigateMode}</p>
       <p>
-
-        <button
-          onClick={() => actions.set('count', (data) => `${data}1`)}
-        >
+        <button onClick={() => actions.set('count', (data) => `${data}1`)}>
           add
         </button>
         <button onClick={() => actions.set('count', undefined)}>
           remove count
         </button>
-        <button onClick={() => actions.clear()}>
-          clear
-        </button>
+        <button onClick={() => actions.clear()}>clear</button>
       </p>
       <p>
-        <button onClick={() => { toggle() }}>toggle navigateMode</button>
+        <button
+          onClick={() => {
+            toggle();
+          }}
+        >
+          toggle navigateMode
+        </button>
       </p>
     </>
   );
